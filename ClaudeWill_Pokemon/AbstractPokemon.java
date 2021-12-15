89780e46694ffa7e9feee6ac8c57cbd9fc7034db
Claude Will <claude.will@yahoo.com>
2021-12-14 23:53:48 -0600

    Another practice program for OOP in progress

---
+++ ClaudeWill_Pokemon/AbstractPokemon.java
@@ -0,0 +1,4 @@
+public abstract class AbstractPokemon implements PokemonInterface {
+    
+    
+}
---
+++ ClaudeWill_Pokemon/Pokemon.java
@@ -0,0 +1,12 @@
+public class Pokemon {
+    private String name;
+    private String type;
+    private int health;
+
+    private static int count = 0;
+
+    public Pokemon
+    void attackPokemon(Pokemon pokemon) {
+        pokemon.health -= 10;
+    }
+}
